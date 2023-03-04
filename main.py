02b0e1542e7bf17fe1b50e7eb7628a9d963c42a9
unknown <gauthier.evraerd@gmail.com>
2023-03-04 21:43:20 +0100

    Algo dpll

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 # https://www2.cs.sfu.ca/CourseCentral/827/havens/papers/topic%237(NoGoodLearning)/Clause%20Watching/ws-sat02-b.pdf
 
 # Objet qui represente une clause, contient la liste des variables de la clauses
+from copy import deepcopy
+
+
 class Clause:
     def __init__(self, list_litteraux):
         # La liste contient les identifiants des variables du dictionnaire
@@ -33,25 +36,74 @@ class Variable:
         else:
             return not self.value
     
+def dpll(data, litteral = None):
+    # litteral sous la forme : "x 1 1 1" ou "!x 1 1 1"
+    clause_empty = []
+    clause_uni = set()
+    if litteral != None:
+        clause_uni.add(Clause([litteral]))
+    # Propagation unitaire
+    # Recherche de toutes les clauses unitaires
+    for key in data:
+        for clause in data[key].clause:
+            if len(clause.list_litteraux) == 1:
+                clause_uni.add(clause)
+    for clause in clause_uni:
+        litt = clause.list_litteraux[0]
+        if "!" in litt:
+            # Met !x à faux
+            data[litt].value.setValue(False)
+            # Supprime toutes les clauses où !x apparait car elles sont satisfaites
+            for clause in data[litt].clause:
+                clause_litt = clause.list_litteraux
+                for l in clause_litt:
+                    data[l].clause.remove(clause)
+            # Supprime x dans toutes les clauses où il apparait
+            inv_litt = litt.split("!")[1]
+            for clause in data[inv_litt].clause:
+                clause.list_litteraux.remove(inv_litt)
+                # Si la clause est vide, on la garde en mémoire
+                if len(clause.list_litteraux) == 0:
+                    clause_empty.append(clause)
+        else:
+            # Met x à vrai
+            data[litt].value.setValue(True)
+            # Supprime toutes les clauses où x apparait car elles sont satisfaites
+            for clause in data[litt].clause:
+                clause_litt = clause.list_litteraux
+                for l in clause_litt:
+                    data[l].clause.remove(clause)
+            # Supprime !x dans toutes les clauses où il apparait
+            inv_litt = "!" + litt
+            for clause in data[inv_litt].clause:
+                clause.list_litteraux.remove(inv_litt)
+                # Si la clause est vide, on la garde en mémoire
+                if len(clause.list_litteraux) == 0:
+                    clause_empty.append(clause)
 
-data = {
-    # Pour la variable à la ligne 1, colonne 1 pour le chiffre 1
-    "x111": {
-        "value": Variable(),
-        # Liste de toutes les clauses dans laquel la variable est présente
-        "clause": []
-    },
-    # Pour la négation de la variable à la ligne 1, colonne 1 pour le chiffre 1
-    "!x111": {
-        "value": Variable(True),
-        "clause": []
-    },
-    "x112": {
-        "value": Variable(),
-        "clause": []
-    },
-    "!x112": {
-        "value": Variable(True),
-        "clause": []
-    }
-}
+    # Test si il y a plus de clause
+    isEmpty = True
+    for key in data:
+        if len(data[key].clause) > 0:
+            isEmpty = False
+    if isEmpty:
+        return data
+    
+    # Test s'il existe des clauses vides
+    if len(clause_empty) > 0:
+        return False
+            
+    # On choisit un litteral, on prends le 1er litteral qui n'a pas encore de valeur associé
+    for key in data:
+        if data[key].value.getValue() == None and len(data[key].clause) > 0:
+            new_litteral = key
+    
+    result = dpll(deepcopy(data), new_litteral)
+    if result:
+        return result
+    result = dpll(deepcopy(data), "!"+new_litteral)
+    if result:
+        return result
+    else:
+        return False
+    
