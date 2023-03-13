aca53b0ea0e1c7b3c76a81914ef03428b9099f48
Gauthier <73186172+HarlausG@users.noreply.github.com>
2023-03-13 22:33:46 +0100

    Resolution dpll

--- main.py
+++ main.py
@@ -2,33 +2,36 @@
 
 # Objet qui represente une clause, contient la liste des variables de la clauses
 from copy import deepcopy
-
+import itertools as it
+import numpy as np
 
 class Clause:
     def __init__(self, list_litteraux):
         # La liste contient les identifiants des variables du dictionnaire
-        self.list_litteraux = list_litteraux
+        self.list_litteraux:list = list_litteraux
         # Une clause est satisfaite si nb_litteraux_satis > 0
         self.nb_litteraux_satis = 0
         # Une clause est insatisfaite si nb_litteraux_nsatis == nb_littéraux de la clause
         self.nb_literraux_nsatis = 0
 
-    def __repr__(self) -> str:
-        return("^".join(self.list_litteraux))
+    #def __repr__(self) -> str:
+        #return("^".join(self.list_litteraux))
     
     def isSatisfy(self):
         pass
 
 # Objet qui represente une variable, contient sa valeur et s'il est une négation ou pas
 class Variable:
-    def __init__(self, isNot = False):
+    def __init__(self, name, isNot = False):
+        # Nom de la variable
+        self.name = name
         # Valeur de la variable, None si pas initialisé, 1 pour vrai, -1 pour faux
         self.value = None
         # Booleen pour savoir si variable est x111 ou !x111 pour calculer sa valeur
         self.isNot = isNot
     
     def __repr__(self) -> str:
-        return(f'Valeur: {self.getValue()}, isNot: {self.isNot}')
+        return(f'Nom: {self.name}, Valeur: {self.getValue()}, isNot: {self.isNot}')
 
     def setValue(self, value):
         self.value = value
@@ -36,15 +39,88 @@ class Variable:
     def getValue(self):
         if not self.isNot or self.value == None:
             return self.value
-        else:
+        else :
             return not self.value
-    
+
+def createDict():
+    data = {}
+    for x in range(1,10):
+        for y in range(1,10):
+            for val in range(1,10):
+                name = str.format("x {} {} {}",x,y,val)
+                data[name] = {"value": Variable(name), "clause": []}
+                data["!"+name] = {"value": Variable("!"+name,True), "clause": []}
+    return data
+
+def genClause(data:dict,listClause:list):
+    for x in range(1,10):
+        for y in range(1,10):
+            eachCell(x,y,data,listClause)
+    for x in range(1,10):
+        for val in range(1,10):
+            eachRow(x,val,data,listClause)
+    for y in range(1,10):
+        for val in range(1,10):
+            eachColumn(y,val,data,listClause)
+    for xSquare in range(1,10,3):
+        for ySquare in range(1,10,3):
+            for val in range(1,10):
+                eachSquare(xSquare,ySquare,val,data,listClause)
+
+# Dans chaque case, il y a un chiffre et un seul
+def eachCell(x:int,y:int,data:dict,list:list):
+    listVar = []
+    listVarNot = []
+    for val in range(1,10):
+        listVar.append(data[str.format("x {} {} {}",x,y,val)]['value'])
+        listVarNot.append(data[str.format("!x {} {} {}",x,y,val)]['value'])
+    createClause(data,listVar,listVarNot,list)
+
+# Dans chaque ligne, chaque chiffre doit apparaître 1 fois et une seule
+def eachRow(x:int,val:int,data:dict,list:list):
+    listVar = []
+    listVarNot = []
+    for y in range(1,10):
+        listVar.append(data[str.format("x {} {} {}",x,y,val)]['value'])
+        listVarNot.append(data[str.format("!x {} {} {}",x,y,val)]['value'])
+    createClause(data,listVar,listVarNot,list)
+
+# Dans chaque colonne, chaque chiffre doit apparaître 1 fois et une seule
+def eachColumn(y:int,val:int,data:dict,list:list):
+    listVar = []
+    listVarNot = []
+    for x in range(1,10):
+        listVar.append(data[str.format("x {} {} {}",x,y,val)]['value'])
+        listVarNot.append(data[str.format("!x {} {} {}",x,y,val)]['value'])
+    createClause(data,listVar,listVarNot,list)
+
+# Dans chaque carré, chaque chiffre doit apparaître 1 fois et une seule
+def eachSquare(xSquare:int,ySquare:int,val:int,data:dict,list:list):
+    listVar = []
+    listVarNot = []
+    for x in range(xSquare,xSquare+3):
+        for y in range(ySquare,ySquare+3):
+            listVar.append(data[str.format("x {} {} {}",x,y,val)]['value'])
+            listVarNot.append(data[str.format("!x {} {} {}",x,y,val)]['value'])
+    createClause(data,listVar,listVarNot,list)
+
+def createClause(data:dict,listVar:list,listVarNot:list,list:list):
+    clause = Clause(listVar)
+    list.append(clause)
+    for var in listVar:
+        data[var.name]['clause'].append(clause)
+    for listClause in it.combinations(listVarNot,2):
+        clause = Clause(listClause)
+        list.append(clause)
+        for var in listClause:
+            data[var.name]['clause'].append(clause)
+
+
 def dpll(data, litteral = None):
-    print("litteral", litteral)
     clause_empty = []
     clause_uni = set()
     if litteral != None:
-        clause_uni.add(Clause([litteral]))
+        clause_uni.add(Clause([data[litteral]["value"]]))
     # Propagation unitaire
     # Recherche de toutes les clauses unitaires
     for key, value in data.items():
@@ -52,26 +128,24 @@ def dpll(data, litteral = None):
             if len(clause.list_litteraux) == 1:
                 clause_uni.add(clause)
     for clause in clause_uni:
-        litt = clause.list_litteraux[0]
+        if len(clause.list_litteraux) > 0:
+            litt = clause.list_litteraux[0].name
         if "!" in litt:
             # Met !x à faux
             data[litt]["value"].setValue(False)
             # Supprime toutes les clauses où !x apparait car elles sont satisfaites
-            for clause in list(data[litt]["clause"]):
+            for clause in data[litt]["clause"]:
                 clause_litt = clause.list_litteraux
                 for l in clause_litt:
-                    data[l]["clause"].remove(clause)
+                    data[l.name]["clause"].remove(clause)
                     
             # Supprime x dans toutes les clauses où il apparait
             inv_litt = litt.split("!")[1]
             data[inv_litt]["value"].setValue(False)
-            for clause in list(data[inv_litt]["clause"]):
+            for clause in data[inv_litt]["clause"]:
                 # On supprimer aussi x des autres clauses car ils sont passé en copy dans le dict
-                """for l in list(clause.list_litteraux):
-                    for clause_l in data[l]["clause"]:
-                        if l in clause_l.list_litteraux:
-                            clause_l.list_litteraux.remove(l)"""
-                clause.list_litteraux.remove(inv_litt)
+                clause.list_litteraux = list(clause.list_litteraux)
+                clause.list_litteraux.remove(data[inv_litt]["value"])
                 # Si la clause est vide, on la garde en mémoire
                 if len(clause.list_litteraux) == 0:
                     clause_empty.append(clause)
@@ -81,27 +155,24 @@ def dpll(data, litteral = None):
             # Met x à vrai
             data[litt]["value"].setValue(True)
             # Supprime toutes les clauses où x apparait car elles sont satisfaites
-            for clause in list(data[litt]["clause"]):
+            for clause in data[litt]["clause"]:
                 clause_litt = clause.list_litteraux
                 for l in clause_litt:
-                    data[l]["clause"].remove(clause)
+                    data[l.name]["clause"].remove(clause)
             # Supprime !x dans toutes les clauses où il apparait
             inv_litt = "!" + litt
             data[inv_litt]["value"].setValue(True)
-            for clause in list(data[inv_litt]["clause"]):
+            for clause in data[inv_litt]["clause"]:
                 # On supprimer aussi x des autres clauses car ils sont passé en copy dans le dict
-                """for l in list(clause.list_litteraux):
-                    for clause_l in data[l]["clause"]:
-                        if l in clause_l.list_litteraux:
-                            clause_l.list_litteraux.remove(l)"""
-                clause.list_litteraux.remove(inv_litt)
+                clause.list_litteraux = list(clause.list_litteraux)
+                clause.list_litteraux.remove(data[inv_litt]["value"])
                 # Si la clause est vide, on la garde en mémoire
                 if len(clause.list_litteraux) == 0:
                     clause_empty.append(clause)
             data[inv_litt]["clause"] = []
 
     #### LITTERAUX PURE ####
-    all_values = list(test.values())
+    all_values = list(data.values())
     for i in range(0,len(all_values), 2):
         x_values = all_values[i]
         notX_values = all_values[i+1]
@@ -111,14 +182,16 @@ def dpll(data, litteral = None):
             for clause in list(notX_values["clause"]):
                 clause_litt = clause.list_litteraux
                 for l in clause_litt:
-                    data[l]["clause"].remove(clause)
+                    if len(data[l.name]["clause"]) > 0:
+                        data[l.name]["clause"].remove(clause)
         elif len(x_values["clause"]) > 0 and len(notX_values["clause"]) == 0:
             x_values["value"].setValue(True)
             # Supprime toutes les clauses où x apparait car elles sont satisfaites
             for clause in list(x_values["clause"]):
                 clause_litt = clause.list_litteraux
                 for l in clause_litt:
-                    data[l]["clause"].remove(clause)
+                    if len(data[l.name]["clause"]) > 0:
+                        data[l.name]["clause"].remove(clause)
 
     # Test si il y a plus de clause
     isEmpty = True
@@ -147,42 +220,31 @@ def dpll(data, litteral = None):
     else:
         return False
 
-#### ENSEMBLE DE CLAUSE DE TEST #####
-c1 = Clause(["x 1", "x 2", "!x 3"])
-c2 = Clause(["!x 1", "x 2", "x 3"])
-c3 = Clause(["!x 2"])
-c4 = Clause(["x 2", "x 3"])
-c5 = Clause(["x 1", "!x 2"])
-test = {
-    "x 1":{
-        "value": Variable(),
-        "clause": [c1, c5]
-    },
-    "!x 1":{
-        "value": Variable(True),
-        "clause": [c2]
-    },
-    "x 2":{
-        "value": Variable(),
-        "clause": [c1, c2, c4]
-    },
-    "!x 2":{
-        "value": Variable(True),
-        "clause": [c3, c5]
-    },
-    "x 3":{
-        "value": Variable(),
-        "clause": [c2, c4]
-    },
-    "!x 3":{
-        "value": Variable(True),
-        "clause": [c1]
-    }
-}
+
+data = createDict()
+listClause = []
+genClause(data,listClause)
+print("Le nombre de variable propositionnelles est de {} et le nombre de clause est de {}".format(int(data.__len__()/2),listClause.__len__()))
+litt = ["x 1 1 8", "x 8 1 7", "x 3 2 6", "x 5 2 1", "x 8 2 5", "x 9 2 3", "x 2 3 4", "x 4 3 6", "x 5 4 8", "x 7 4 4", "x 3 5 3", "x 7 5 7", 
+                          "x 2 6 2", "x 6 6 5", "x 8 6 3","x 9 6 8", "x 7 7 8", "x 3 8 4", "x 5 8 5", "x 8 8 6", "x 9 8 1", "x 1 9 9", "x 6 9 2"]
+for l in litt:
+    clause = Clause([data[l]["value"]])
+    data[l]["clause"].append(clause)
+    listClause.append(clause)
 
 
-result_dpll = dpll(test)
+
+result_dpll = dpll(data)
 if not result_dpll:
     print("Pas de modele")
 else:
-    print(result_dpll)
+    print("Un modele")
+
+m = np.zeros((9,9))
+for i in range(1,10):
+    for j in range(1,10):
+        for k in range(1,10):
+            if result_dpll[str.format("x {} {} {}",i,j,k)]["value"].value:
+                m[j-1][i-1] = k
+                break
+print(m)
